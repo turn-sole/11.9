61b3be8c025001400a6d18e47d1b3dddbf06bcb2
turn-sole <113235914+turn-sole@users.noreply.github.com>
2022-11-09 18:59:47 +0800

    Add files via upload

---
+++ dfjkls.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int b[] = { 2,3,4,5,6 };
+//	int c[] = { 3,4,5,6,7 };
+//	int* arr[3] = { a,b,c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			/*printf("%d ",*(arr[i]+j));*/
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//void test(int arr[3][5])
+//{}
+//void test(int arr[][])
+//{}
+//void test(int arr[][5])
+//{}
+//void test(int*arr)
+//{}
+//void test(int* arr[5])
+//{}
+//void test(int (*arr)[5])
+//{}
+//void test(int **arr)
+//{}
+//int main()
+//{
+//	int arr[3][5] = { 0 };
+//	test(arr);
+//}
